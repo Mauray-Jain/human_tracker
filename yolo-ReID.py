31575d0e45440dbace1e8756b43af5f2f435485f
DeveloperAkansh26 <gupta.akansh26@gmail.com>
2025-02-18 01:59:53 +0530

    multi-person not working

--- yolo-ReID.py
+++ yolo-ReID.py
@@ -15,7 +15,7 @@ from tqdm import tqdm
 DATA_DIR = "/home/akansh_26/Hackathons/VisionX-AI/Person-Re-Id-Dataset/train"
 DEVICE = "cuda"
 
-MAIN_ENCODING = None
+MAIN_ENCODING = {}
 # STATE = "PICK" # PICK / TRACK
 # user_input = None
 count = 0
@@ -31,6 +31,13 @@ cam2 = cv2.VideoCapture(URL2)
 
 
 def main():
+    # ret1, frame1 = cam1.read()
+    # ret2, frame2 = cam2.read()
+
+    # cv2.imshow("Camera 1", frame1)
+    # cv2.imshow("Camera 2", frame2)
+    # camid = input("Enter which cam you want to select: ")
+    
     while True:
         ret1, frame1 = cam1.read()
         ret2, frame2 = cam2.read()
@@ -39,7 +46,7 @@ def main():
             break
 
         annotated_frame1 = process_frame(frame1, model_yolo1, 1)
-        annotated_frame2 = process_frame(frame2, model_yolo2, 2)
+        annotated_frame2 = process_frame(frame2, model_yolo1, 2)
 
         cv2.imshow("frame1", annotated_frame1)
         cv2.imshow("frame2", annotated_frame2)
@@ -54,34 +61,42 @@ def process_frame(frame, model_yolo, camera_number):
 
     dist = 1000000
 
-    results = model_yolo(frame, verbose=False)
-    distances = []
-    bounding_boxes_list = []
+    results = model_yolo.track(frame, persist=True, tracker="botsort.yaml")
 
-    for result in results:
-        for box, cls, conf in zip(result.boxes.xyxy, result.boxes.cls, result.boxes.conf):
-            if cls != 0:
-                continue
-            
+    boxes = results[0].boxes.xywh.cpu().numpy()  # Format: [x, y, width, height]
+    confs = results[0].boxes.conf.cpu().numpy()   # Confidence scores
+    classes = results[0].boxes.cls.cpu().numpy()    # Class labels (as indices)
+    track_ids = results[0].boxes.id.cpu().numpy() if results[0].boxes.id is not None else None
+
+    threshold = 4
+
+    if camera_number == 1:
+
+        if boxes is not None and boxes.any():
             count += 1
 
-            x1, y1, x2, y2 = box
-            pt1 = (int(x1), int(y1))
-            pt2 = (int(x2), int(y2))
-            
-            if count == 1:
-                img_person = frame[int(y1):int(y2), int(x1):int(x2)]
-                img_person = cv2.resize(img_person, (128, 64))
-                img_person = torch.from_numpy(img_person).permute(2, 0, 1) / 255.0
-                model_siamese.eval()
-                with torch.no_grad():
-                    img_person = img_person.to(DEVICE)
-                    MAIN_ENCODING = model_siamese(img_person.unsqueeze(0))
-                    MAIN_ENCODING = MAIN_ENCODING.detach().cpu().numpy()
-                    print(f"Enc ID-1: {MAIN_ENCODING}")
+        if count == 1:
+            for i, box in enumerate(boxes):
+                cls = int(classes[i])
+                if cls != 0:
+                    continue
 
-            if MAIN_ENCODING is not None and MAIN_ENCODING.any():
-                img_person = frame[int(y1):int(y2), int(x1):int(x2)]
+                tid = int(track_ids[i]) if track_ids is not None else -1
+                conf = confs[i]
+
+                x, y, w, h = box
+                pt1 = (int(x-w//2), int(y-h//2))
+                pt2 = (int(x+w//2), int(y+h//2))
+
+                # if tid == 1 and -1 < count < 100 and camera_number == 1:
+                #     img_person = frame[int(x-w//2):int(x+w//2), int(y-h//2):int(y+h//2)]
+                #     img_person = torch.from_numpy(img_person).permute(2, 0, 1) / 255.0
+                #     img_person = img_person.to(DEVICE)
+                #     MAIN_ENCODING = model_siamese(img_person.unsqueeze(0))
+                #     MAIN_ENCODING = MAIN_ENCODING.detach().cpu().numpy()
+                #     print(f"Enc ID-1: {MAIN_ENCODING}")
+
+                img_person = frame[int(y-h//2):int(y+h//2), int(x-w//2):int(x+w//2)]
                 img_person = cv2.resize(img_person, (128, 64))
                 img_person = torch.from_numpy(img_person).permute(2, 0, 1) / 255.0
                 model_siamese.eval()
@@ -90,29 +105,84 @@ def process_frame(frame, model_yolo, camera_number):
                     person_enc = model_siamese(img_person.unsqueeze(0))
                     person_enc = person_enc.detach().cpu().numpy()
 
-                dist = euclidean_dist(MAIN_ENCODING, person_enc)
+                MAIN_ENCODING[tid] = person_enc
+        
+        else:
+            
+            for i, box in enumerate(boxes):
+                cls = int(classes[i])
+                if cls != 0:
+                    continue
 
-                threshold = 3
+                tid = int(track_ids[i]) if track_ids is not None else -1
+                conf = confs[i]
+
+                x, y, w, h = box
+                pt1 = (int(x-w//2), int(y-h//2))
+                pt2 = (int(x+w//2), int(y+h//2))
+
+                img_person = frame[int(y-h//2):int(y+h//2), int(x-w//2):int(x+w//2)]
+                img_person = cv2.resize(img_person, (128, 64))
+                img_person = torch.from_numpy(img_person).permute(2, 0, 1) / 255.0
+                model_siamese.eval()
+                with torch.no_grad():
+                    img_person = img_person.to(DEVICE)
+                    person_enc = model_siamese(img_person.unsqueeze(0))
+                    person_enc = person_enc.detach().cpu().numpy()
+
+                distances = []
+                ids_list = []
+                for i in MAIN_ENCODING:
+                    dist = euclidean_dist(MAIN_ENCODING[i], person_enc)
+                    if dist < threshold:
+                        distances.append(dist)
+                        ids_list.append(i)
+                
+                try:
+                    idx = np.argmin(distances)
+                    cv2.putText(frame, f"ID:{ids_list[idx]}", (int(x-w//2), int(y-h//2)-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                except:
+                    pass
+                
+                cv2.rectangle(frame, pt1, pt2, color=(0, 255, 0), thickness=2)
+    else:
+        for i, box in enumerate(boxes):
+            cls = int(classes[i])
+            if cls != 0:
+                continue
+
+            tid = int(track_ids[i]) if track_ids is not None else -1
+            conf = confs[i]
+
+            x, y, w, h = box
+            pt1 = (int(x-w//2), int(y-h//2))
+            pt2 = (int(x+w//2), int(y+h//2))
+
+            img_person = frame[int(y-h//2):int(y+h//2), int(x-w//2):int(x+w//2)]
+            img_person = cv2.resize(img_person, (128, 64))
+            img_person = torch.from_numpy(img_person).permute(2, 0, 1) / 255.0
+            model_siamese.eval()
+            with torch.no_grad():
+                img_person = img_person.to(DEVICE)
+                person_enc = model_siamese(img_person.unsqueeze(0))
+                person_enc = person_enc.detach().cpu().numpy()
+
+            distances = []
+            ids_list = []
+            for i in MAIN_ENCODING:
+                dist = euclidean_dist(MAIN_ENCODING[i], person_enc)
                 if dist < threshold:
                     distances.append(dist)
-                    bounding_boxes_list.append(box)
+                    ids_list.append(i)
+            
+            try:
+                idx = np.argmin(distances)
+                cv2.putText(frame, f"ID:{ids_list[idx]}", (int(x-w//2), int(y-h//2)-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            except:
+                pass
 
-    if len(distances) == 1:
-        x1, y1, x2, y2 = bounding_boxes_list[0]
-        pt1 = (int(x1), int(y1))
-        pt2 = (int(x2), int(y2))
-        cv2.rectangle(frame, pt1, pt2, color=(0, 255, 0), thickness=2)
-        cv2.putText(frame, f"ID:{1}", (int(x1), int(y1)-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-    else:
-        try:
-            idx = np.argmin(distances)
-            x1, y1, x2, y2 = bounding_boxes_list[idx]
-            pt1 = (int(x1), int(y1))
-            pt2 = (int(x2), int(y2))
             cv2.rectangle(frame, pt1, pt2, color=(0, 255, 0), thickness=2)
-            cv2.putText(frame, f"ID:{1}", (int(x1), int(y1)-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        except:
-            pass
+            
 
     return frame
 
